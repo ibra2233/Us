c8a53cee268175f0a070375c837a1897c2d1e7b9
ibra2233 <35203482+ibra2233@users.noreply.github.com>
2026-01-15 00:30:03 +0100

    capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.logitrack.user',
+  appId: 'com.usertrak.user',
   appName: 'Trackuser',
   webDir: 'dist',
   bundledWebRuntime: false,
