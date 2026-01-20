df3362ce0236f4173cce24dab2b1bdbfd11f813b
ibra2233 <35203482+ibra2233@users.noreply.github.com>
2026-01-20 20:13:46 +0100

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,21 @@
+
+/**
+ * LogiTrack Configuration
+ */
+
+export const DB_CONFIG = {
+  enabled: true,
+  url: 'https://zdcngosnxowrycqtmvkt.supabase.co',
+  publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkY25nb3NueG93cnljcXRtdmt0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1MDMzODYsImV4cCI6MjA4NDA3OTM4Nn0.rk7uRggTf_1qViBDrrYqooveENckIYZYkuL0DqEfeeo',
+  secretKey: ''
+};
+
+// كلمة مرور الدخول للوحة الإدارة
+export const ADMIN_PASSWORD = '123'; 
+
+/**
+ * اختر نوع التطبيق الذي تريد بناؤه الآن:
+ * 'customer' -> تطبيق تتبع للزبائن فقط (واجهة واحدة مباشرة)
+ * 'admin'    -> تطبيق لوحة تحكم للشركة فقط
+ */
+export const APP_TYPE: 'admin' | 'customer' = 'customer'; 
